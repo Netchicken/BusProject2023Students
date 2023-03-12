e193474d95fbd80c358283bbad00b6dce0b6bece
Gary <Gary.Dix@enform.nz>
2023-03-13 10:51:40 +1300

    Add project files.

---
+++ DTO/BusData.cs
@@ -0,0 +1,50 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BusProject2023Students.DTO
+{
+    public class BusData
+    {
+        //Prices
+        [Display(Name = "Ashburton")]
+        public double Ash { get; set; } = 17.99;
+        [Display(Name = "Timaru")]
+        public double Tim { get; set; } = 24.77;
+        [Display(Name = "Oamaru")]
+        public double Om { get; set; } = 24.44;
+        [Display(Name = "Dunedin")]
+        public double Dun { get; set; } = 35.66;
+        [Display(Name = "Invercargil")]
+
+        //Discounts
+        public double Inv { get; set; } = 46.00;
+        [Display(Name = "Fare Discount")]
+
+        public double Discount { get; set; } = 0.20;
+        [Display(Name = "What is your Age?")]
+        public int Age { get; set; }
+
+        [Display(Name = "Are you Disabled?")]
+        public bool IsDisabled { get; set; } = false;
+        [Display(Name = "Is this the Weekend?")]
+        public bool IsWeekend { get; set; } = false;
+
+
+        //Details
+
+        [Display(Name = "First Name")]
+        public string? FName { get; set; }
+        [Display(Name = "Last Name")]
+        public string? LName { get; set; }
+
+
+
+
+        //Not shown on form
+        [Display(Name = "Lower Age Discount")]
+        public int lowerAgeDiscount { get; set; } = 12;
+        [Display(Name = "Upper Age Discount")]
+        public int UpperAgeDiscount { get; set; } = 64;
+
+
+    }
+}
---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,26 @@
+﻿using BusProject2023Students.DTO;
+
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BusProject2023Students.Pages
+{
+    [BindProperties]
+    public class IndexModel : PageModel
+    {
+
+        public BusData busData { get; set; }
+
+
+        public void OnGet()
+        {
+
+        }
+
+        public void OnPost()
+        {
+
+
+        }
+    }
+}
